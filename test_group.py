dc8aeb1662310976a8db28fec6e4bac3540c7b89
goncharov.vv <goncharov.vv@esphere.ru>
2024-08-13 23:06:45 +0300

    Add file application and move some def to file. Test  OK

--- test_group.py
+++ test_group.py
@@ -3,73 +3,31 @@ from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 from group import Group
+from application import Application
 import unittest
 
 
 class UntitledTestCase(unittest.TestCase):
     def setUp(self):
-        self.wd = webdriver.Chrome()
-        self.wd.implicitly_wait(30)
+        self.app = Application()
     
     def test_add_group(self):
-        self.open_home_page()
-        self.login(username="admin", password="secret")
-        self.open_group_page()
-        self.create_group(Group(name="fqwefaef", header="faewf", footer="faf"))
-        self.return_to_groups_page()
-        self.logout()
+        self.app.open_home_page()
+        self.app.login(username="admin", password="secret")
+        self.app.open_group_page()
+        self.app.create_group(Group(name="fqwefaef", header="faewf", footer="faf"))
+        self.app.return_to_groups_page()
+        self.app.logout()
 
     def test_add_empty_group(self):
-        wd = self.wd
-        self.open_home_page()
-        self.login(username="admin", password="secret")
-        self.open_group_page()
-        self.create_group(Group(name="", header="", footer=""))
-        self.return_to_groups_page()
-        self.logout()
+        self.app.open_home_page()
+        self.app.login(username="admin", password="secret")
+        self.app.open_group_page()
+        self.app.create_group(Group(name="", header="", footer=""))
+        self.app.return_to_groups_page()
+        self.app.logout()
 
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
 
-    def return_to_groups_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("group page").click()
-
-    def create_group(self, group):
-        wd = self.wd
-        # init group creation
-        wd.find_element_by_name("new").click()
-        # fill group firm
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit group creation
-        wd.find_element_by_name("submit").click()
-
-    def open_group_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("groups").click()
-
-    def login(self, username, password):
-        wd = self.wd
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-
-    def open_home_page(self):
-        wd = self.wd
-        wd.get("http://localhost/addressbook/")
 
     def is_element_present(self, how, what):
         try: self.wd.find_element(by=how, value=what)
@@ -82,7 +40,7 @@ class UntitledTestCase(unittest.TestCase):
         return True
 
     def tearDown(self):
-        self.wd.quit()
+        self.app.destroy()
 
 
 if __name__ == "__main__":
