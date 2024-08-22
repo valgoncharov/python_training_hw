170dc50b40211eec299889a2d5f3be7e8a1b415e
goncharov.vv <goncharov.vv@esphere.ru>
2024-08-22 19:58:12 +0300

    Add list in test and assert. Test ok

---
+++ test/test_add_group.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from model.group import Group
+
+
+def test_add_group(app):
+    old_groups = app.group.get_group_list()
+    app.group.create(Group(name="fqwefaef", header="faewf", footer="faf"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
+
+
+def test_add_empty_group(app):
+    old_groups = app.group.get_group_list()
+    app.group.create(Group(name="", header="", footer=""))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
