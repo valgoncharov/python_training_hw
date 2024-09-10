dfb8688c55826f4f5b7fa27bca838ecb49635518
goncharov.vv <goncharov.vv@esphere.ru>
2024-09-10 22:24:20 +0300

    Add new user test - homework.

--- fixture/application.py
+++ fixture/application.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
+from fixture.contact import ContactHelper
 
 
 class Application:
@@ -9,6 +10,7 @@ class Application:
         self.wd = webdriver.Chrome()
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
+        self.contact = ContactHelper(self)
 
     def is_valid(self):
         try:
