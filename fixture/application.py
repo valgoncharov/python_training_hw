7955b051249f342e99032631600c8979971aaa4e
goncharov.vv <goncharov.vv@esphere.ru>
2024-09-12 19:35:15 +0300

    Update tests - add fixture. Test Ok. Homework.

--- fixture/application.py
+++ fixture/application.py
@@ -25,3 +25,71 @@ class Application:
 
     def destroy(self):
         self.wd.quit()
+
+    def logout(self):
+        wd = self.wd
+        # loguot
+        wd.find_element_by_link_text("Logout").click()
+
+    def return_to_home_page(self):
+        wd = self.wd
+        # Go to home page ?
+        wd.find_element_by_link_text("home page").click()
+
+    def submit_new_user(self):
+        wd = self.wd
+        # submit new user
+        wd.find_element_by_xpath("//div[@id='content']/form/input[20]").click()
+        self.return_to_home_page()
+
+    def fill_new_user(self, user):
+        wd = self.wd
+        self.open_add_new_page()
+        # fill new user
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(user.firstname)
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(user.middlename)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(user.lastname)
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(user.address)
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(user.homephone)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(user.mobilephone)
+        wd.find_element_by_name("work").click()
+        wd.find_element_by_name("work").clear()
+        wd.find_element_by_name("work").send_keys(user.workphone)
+        self.submit_new_user()
+
+    def open_add_new_page(self):
+        wd = self.wd
+        # Open add new page
+        wd.find_element_by_link_text("add new").click()
+        # wd.get("http://localhost/addressbook/edit.php")
+
+    def login(self, username, password):
+        wd = self.wd
+        # login
+        self.open_home_page()
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_home_page(self):
+        wd = self.wd
+        # Open home page
+        wd.get("http://localhost/addressbook/")
+        # wd.get("http://localhost/addressbook/index.php")
+
--- test/test_contact_hw.py
+++ test/test_contact_hw.py
@@ -1,118 +1,23 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest
+import pytest
 from user import User
+from fixture.application import Application
 
 
-class UntitledTestCase(unittest.TestCase):
-    def setUp(self):
-        self.wd = webdriver.Chrome()
-        self.wd.implicitly_wait(30)
-
-    def test_add_new_user(self):
-        wd = self.wd
-        self.login(wd, username="admin", password="secret")
-        self.fill_new_user(wd, User(firstname="iewrfnse", middlename="fsf", lastname="Poigl", address="str. Uhhfjfd", homephone="+5-585-6894", mobilephone="(99)-43-656", workphone="32425346"))
-        self.logout(wd)
-
-    def test_add_empty_user(self):
-        wd = self.wd
-        self.login(wd, username="admin", password="secret")
-        self.fill_new_user(wd, User(firstname="", middlename="", lastname="", address="", homephone="", mobilephone="", workphone=""))
-        self.logout(wd)
-
-    def logout(self, wd):
-        # loguot
-        wd.find_element_by_link_text("Logout").click()
-
-    def return_to_home_page(self, wd):
-        # Go to home page ?
-        wd.find_element_by_link_text("home page").click()
-
-    def submit_new_user(self, wd):
-        # submit new user
-        wd.find_element_by_xpath("//div[@id='content']/form/input[20]").click()
-        self.return_to_home_page(wd)
-
-    def fill_new_user(self, wd, user):
-        self.open_add_new_page(wd)
-        # fill new user
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(user.firstname)
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(user.middlename)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(user.lastname)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(user.address)
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(user.homephone)
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(user.mobilephone)
-        wd.find_element_by_name("work").click()
-        wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(user.workphone)
-        self.submit_new_user(wd)
-
-    def open_add_new_page(self, wd):
-        # Open add new page
-        wd.find_element_by_link_text("add new").click()
-        # wd.get("http://localhost/addressbook/edit.php")
-
-    def login(self, wd, username, password):
-        # login
-        self.open_home_page(wd)
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-
-    def open_home_page(self, wd):
-        # Open home page
-        wd.get("http://localhost/addressbook/")
-
-    # wd.get("http://localhost/addressbook/index.php")
-
-    def is_element_present(self, how, what):
-        try:
-            self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e:
-            return False
-        return True
-
-    def is_alert_present(self):
-        try:
-            self.wd.switch_to_alert()
-        except NoAlertPresentException as e:
-            return False
-        return True
-
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.wd.switch_to_alert()
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally:
-            self.accept_next_alert = True
-
-    def tearDown(self):
-        self.wd.quit()
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_add_new_user(app):
+    app.login(username="admin", password="secret")
+    app.fill_new_user(User(firstname="iewrfnse", middlename="fsf", lastname="Poigl", address="str. Uhhfjfd", homephone="+5-585-6894", mobilephone="(99)-43-656", workphone="32425346"))
+    app.logout()
+
+
+def test_add_empty_user(app):
+    app.login(username="admin", password="secret")
+    app.fill_new_user(User(firstname="", middlename="", lastname="", address="", homephone="", mobilephone="", workphone=""))
+    app.logout()
