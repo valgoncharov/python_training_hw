58d7338c7b8a31e148a11e4d501b9056dcb28875
goncharov.vv <goncharov.vv@esphere.ru>
2024-09-12 18:54:32 +0300

    Add new class User. Test Ok. Homework.

--- user.py
+++ user.py
@@ -1,6 +1,7 @@
 
 
 class User:
+
     def __init__(self, firstname, middlename, lastname, address, homephone, mobilephone, workphone):
         self.firstname = firstname
         self.middlename = middlename
