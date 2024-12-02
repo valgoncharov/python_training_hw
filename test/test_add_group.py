838ac9cfc7fc9ce808ee1cb2f6ba6f4ca44732df
valgoncharov <99090572+valgoncharov@users.noreply.github.com>
2024-12-02 22:17:59 +0300

    Make test faster

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -2,36 +2,10 @@
 from model.group import Group
 
 
-
-# testdata = [
-#     Group(name=name, header=header, footer=footer)
-#     for name in ["", random_string("name", 10)]
-#     for header in ["", random_string("header", 20)]
-#     for footer in ["", random_string("footer", 20)]
-# ]
-
-# testdata = [
-#     Group(name="fqwefaef", header="faewf", footer="faf"),
-#     Group(name="", header="", footer="")
-# ]
-
-
-#@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
-def test_add_group(app, json_groups):
+def test_add_group(app, db, json_groups):
     group = json_groups
-    old_groups = app.group.get_group_list()
+    old_groups = db.get_group_list()
     app.group.create(group)
-    assert len(old_groups) + 1 == app.group.count()
-    new_groups = app.group.get_group_list()
+    new_groups = db.get_group_list()
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-
-
-# def test_add_empty_group(app):
-#     old_groups = app.group.get_group_list()
-#     group =
-#     app.group.create(group)
-#     new_groups = app.group.get_group_list()
-#     assert len(old_groups) + 1 == len(new_groups)
-#     old_groups.append(group)
-#     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
