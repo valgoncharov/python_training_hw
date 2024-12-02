e071ca4417e1b547674c0106b9569868578addf7
valgoncharov <99090572+valgoncharov@users.noreply.github.com>
2024-12-02 22:39:04 +0300

    Change test

--- fixture/db.py
+++ fixture/db.py
@@ -1,6 +1,6 @@
 import mysql.connector
 from model.group import Group
-
+from model.contact import Contact
 
 class DbFixture:
 
@@ -15,10 +15,22 @@ class DbFixture:
         list = []
         cursor = self.connection.cursor()
         try:
-            cursor.execute("select group_id, group_name, group_header, group_footer, from group_list")
+            cursor.execute("select group_id, group_name, group_header, group_footer, from group_list where deprecated='0000-00-00 00:00:00'")
             for row in cursor:
                 (id, name, header, footer) = row
                 list.append(Group(id=str(id), name=name, header=header, footer=footer))
+        finally:
+            cursor.close()
+        return list
+
+    def get_contact_list(self):
+        list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select id, firstname, lastname from addressbook")
+            for row in cursor:
+                (id, firstname, lastname) = row
+                list.append(Contact(id=str(id), firstname=firstname, lastname=lastname))
         finally:
             cursor.close()
         return list
