c887230824406103ac03639473245a1280924cd6
valgoncharov <99090572+valgoncharov@users.noreply.github.com>
2024-12-05 20:55:52 +0300

    Add new funtcions
    Add ORM

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,12 +1,12 @@
-import mysql.connector
-from fixture.db import DbFixture
+from fixture.orm import ORMFixture
+from model.group import Group
 
-db = DbFixture(host="127.0.0.1", name="addressbook", user="root", password="")
+db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 
 try:
-   contacts = db.get_contact_list()
-   for contact in contacts:
-       print(contact)
-   print(len(contacts))
+   l = db.get_contacts_not_in_group(Group(id="11")) #get_group_list #get_contact_list #get_contacts_in_group
+   for item in l:
+       print(item)
+   print(len(l))
 finally:
-    db.destroy()
+    pass #db.destroy()
